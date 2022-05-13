ac2451f20357aa4652ede94456725361a6e464ec
MithunCh <mithun.howlader222@gmail.com>
2022-05-13 18:30:33 +0600

    linq query practices

---
+++ LinqQueryExample/LinqQuery/Cricket.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqQuery
+{
+    public class Cricket
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public double Age { get; set; }
+        public Gender Sex { get; set; }  
+        public string Dept { get; set; }
+        public int ProjectId { get; set; }
+        public Project Project { get; set; }
+
+        public static List<Cricket> student_data = new List<Cricket>();
+
+        public void CricketData()
+        {
+            student_data.Clear();
+            student_data.Add(new Cricket { ProjectId = 1, Name = "Mizanur Rahaman", Id = 1, Dept = "CSE", Age = 25, Sex = Gender.Male });
+            student_data.Add(new Cricket { ProjectId = 1, Name = "Mithun ali", Id = 3, Age = 26, Dept = "CSE", Sex = Gender.Male });
+            student_data.Add(new Cricket { ProjectId = 1, Name = "Samsur rahaman", Id = 5, Dept = "CSE", Age = 24, Sex = Gender.Male });
+            student_data.Add(new Cricket { ProjectId = 1, Name = "Chaity", Id = 8, Dept = "CSE", Age = 20, Sex = Gender.Female });
+            student_data.Add(new Cricket { ProjectId = 1, Name = "Mustafizur rahaman", Id = 4, Age = 29, Dept = "CSE", Sex = Gender.Male });
+            student_data.Add(new Cricket { ProjectId = 1, Name = "M Rahaman", Id = 10, Age = 20, Dept = "CSE", Sex = Gender.Other });
+            student_data.Add(new Cricket { ProjectId = 1, Name = "Mithun CH", Id = 13, Age = 22, Dept = "CSE", Sex = Gender.Male });
+            student_data.Add(new Cricket { ProjectId = 1, Name = "kopa rahaman", Id = 15, Age = 25, Dept = "CSE", Sex = Gender.Other });
+            student_data.Add(new Cricket { ProjectId = 1, Name = "kuddus rahaman", Id = 18, Dept = "CSE", Age = 24, Sex = Gender.Male });
+            student_data.Add(new Cricket { ProjectId = 1, Name = "kakoli", Id = 14, Dept = "CSE", Age = 25, Sex = Gender.Female });
+        }
+
+        public enum Gender
+        {
+            Male = 1,
+            Female = 2,
+            Other = 3
+        }
+    }
+}
---
+++ LinqQueryExample/LinqQuery/Program.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqQuery
+{
+    internal class Program
+    {
+        public static List<Employee> employees = new List<Employee>();
+        public static List<Project> projects = new List<Project>();
+        static void Main(string[] args)
+        {
+            List<int> number = new() { 2, 4, 6, 7, 8, 9 };
+
+
+            //query syntax
+            IEnumerable<int> filteringQuery = from num in number
+                                              where num < 3 || num > 8
+                                              orderby num descending
+                                              select num;
+            foreach (int item in filteringQuery)
+            {
+                //Console.WriteLine(item);
+            }
+
+            //method syntax
+            List<int> number2 = new() { 10, 25, 18, 20, 22, 17, 11 };
+            var avarage = number2.Average();
+            //Console.WriteLine(avarage);
+
+            var contact = number.Concat(number2);
+            foreach (var item in contact)
+            {
+                //Console.Write($"{item},");
+            }
+
+            IEnumerable<int> largeNumber = number2.Where(c => c > 17);
+            foreach (var item in largeNumber)
+            {
+                //Console.WriteLine(item);                                            
+            }
+
+            //mixed query
+            // Using a query expression with method syntax
+            var largeNum = (from num in number2
+                            where num > 20
+                            select num).Count();
+            //Console.WriteLine(largeNum);
+
+            // Better: Create a new variable to store
+            // the method call result
+
+            IEnumerable<int> smallNumber = number2.Where(c => c < 11);
+            IEnumerable<int> samllNumber1 = from num in number2
+                                            where num < 11
+                                            select num;
+
+            foreach (var item in samllNumber1)
+            {
+                //Console.WriteLine(item);
+            }
+
+            var samllNumberCount = samllNumber1.Count();
+            //Console.WriteLine(samllNumberCount);
+
+            InitializeEmployee();
+            InitializeProject();
+
+            //query syntax
+            //var query = (from name in employees
+            //                    select name).Count();
+
+            //method syntax
+            //var query = employees.OrderBy(x => x.EmployeeName).Count();
+            //Console.WriteLine(query);
+
+
+            //order by
+            //var query = from emp in employees
+            //            orderby emp.EmployeeName descending
+            //            select emp;
+
+            //var query = employees.OrderByDescending(x => x.EmployeeName);
+            //var query = employees.OrderBy(x => x.EmployeeName);
+
+            //var query = from emp in employees
+            //            orderby emp.EmployeeName descending, emp.EmployeeId ascending
+            //            select emp;
+
+            //var query = employees.OrderBy(x => x.EmployeeName).ThenByDescending(x => x.EmployeeId);
+
+            //var query = from emp in employees
+            //            where emp.EmployeeName.StartsWith("M") && emp.EmployeeName.EndsWith("n")
+            //            select emp;
+
+            //var query = employees.Where(x => x.EmployeeName.StartsWith("M") || x.EmployeeName.EndsWith("n"));
+
+            //-----Take
+            //var query = (from emp in employees
+            //             select emp).Take(2);
+
+            //var query = employees.Take(4);
+
+            //-------skip
+            //var query = (from emp in employees
+            //             select emp).Skip(2);
+
+            //var query = employees.Skip(3);
+
+            //-----group
+            //var query = from emp in employees
+            //            group emp by emp.ProjectId;
+
+            //var query = employees.GroupBy(x => x.ProjectId);
+            //foreach (var item in query)
+            //{
+            //    Console.WriteLine($"{item.Key}, {item.Count()}");
+            //    //Console.WriteLine($"{item.EmployeeName}, {item.EmployeeId}");
+            //}
+
+            //join
+            //var query = from emp in employees
+            //            join project in projects on emp.ProjectId equals project.ProjectId
+            //            select new { emp.EmployeeName, project.ProjectName };
+
+            var query = employees.Join(projects, e => e.ProjectId, p => p.ProjectId,
+                (e, p) => new {e.EmployeeName, p.ProjectName});
+
+            foreach (var item in query)
+            {
+                Console.WriteLine($"{item.EmployeeName}, {item.ProjectName}");
+            }
+
+
+            //foreach (var item in query)
+            //{
+            //    Console.WriteLine($"{item.EmployeeName}, {item.EmployeeId}");
+            //}
+
+        }
+
+        public static void InitializeEmployee()
+        {
+            employees.Add(new Employee()
+            {
+                EmployeeId = 1,
+                EmployeeName = "Mizan",
+                ProjectId = 1,
+            });
+
+            employees.Add(new Employee()
+            {
+                EmployeeId = 2,
+                EmployeeName = "Ripon",
+                ProjectId = 1,
+            });
+
+            employees.Add(new Employee()
+            {
+                EmployeeId = 3,
+                EmployeeName = "Mithun",
+                ProjectId = 2,
+            });
+
+            employees.Add(new Employee()
+            {
+                EmployeeId = 4,
+                EmployeeName = "Rasel",
+                ProjectId = 2,
+            });
+
+            employees.Add(new Employee()
+            {
+                EmployeeId = 5,
+                EmployeeName = "Rasel",
+                ProjectId = 2,
+            });
+        }
+
+        public static void InitializeProject()
+        {
+            projects.Add(new Project() { ProjectId = 1, ProjectName = "DevSkill" });
+            projects.Add(new Project() { ProjectId = 2, ProjectName = "DevTeam" });
+        }
+    }
+}
